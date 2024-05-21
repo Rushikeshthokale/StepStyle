87bed89857d47bbdfd762170f31e524c015e588b
Rushikesh Thokale <87907210+Rushikeshthokale@users.noreply.github.com>
2024-05-21 12:36:58 +0530

    Update settings.py

--- stepstyle/settings.py
+++ stepstyle/settings.py
@@ -91,8 +91,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rushithokale21@gmail.com'
-EMAIL_HOST_PASSWORD = 'fajm sjwr kfpi bqkg'
+EMAIL_HOST_USER = 'admin gmail'
+EMAIL_HOST_PASSWORD = 'email host password'
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
